981ed353f8d2126fdb4b80185e3bd7c2fa4bfbd7
PhelipiM <ggizmeumigo@gmail.com>
2023-04-18 15:07:05 -0300

    Criação-Serial-Livro-Autor

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria, Editora
+from livraria.models import Categoria, Editora, Autor, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -11,4 +11,14 @@ class CategoriaSerializer(ModelSerializer):
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
+        fields = "__all__"
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
         fields = "__all__"
